ccd618a80d08544845d3153b7e07ef620b7a1894
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-10-18 12:15:43 -0500

    passing specs

--- lib/backstop-deploys/web.rb
+++ lib/backstop-deploys/web.rb
@@ -1,7 +1,45 @@
 module Backstop
   module Deploys
     class Web < Sinatra::Base
+      helpers do
+        def api_user
+          CGI.escape(Config.librato_email)
+        end
+
+        def api_pass
+          Config.librato_key
+        end
+
+        def api_endpoint
+          "https://#{api_user}:#{api_pass}@metrics-api.librato.com/v1"
+        end
+
+        def parse_id(id)
+          id.split('.')    
+        end
+      end
+
       put '/deploys/:id' do |id|
+        app, version, start_time = parse_id(id)
+        halt [400, {:message => ':id should be in the format of app.version.epoch'}.to_json] if app.nil? or version.nil? or start_time.nil?
+        end_time = params[:end_time]
+        halt [400, {:message => ':source is a required param'}.to_json] unless source = params[:source]
+        json = RestClient.get "#{api_endpoint}/annotations/deploys", :params => {'sources[0]' => source, :start_time => start_time}
+        data = JSON.parse(json)
+
+        if events = data['events'].first
+          # events already exists, we just need to update
+          event = events[source].first
+          event_id = event['id']
+          payload = { :title => "#{app}.#{version}" }
+          payload[:end_time] = params[:end_time] if params[:end_time]
+          RestClient.put "#{api_endpoint}/annotations/deploys/#{event_id}", payload
+        else
+          # new event needs to be created
+          payload = { :start_time => start_time, :title => "#{app}.#{version}" }
+          payload[:end_time] = end_time if end_time
+          RestClient.put "#{api_endpoint}/annotations/deploys", "title=#{app}.#{version}&start_time=#{start_time}"
+        end
       end
     end
   end
--- spec/backstop-deploys/web_spec.rb
+++ spec/backstop-deploys/web_spec.rb
@@ -8,13 +8,16 @@ describe Backstop::Deploys::Web do
     Backstop::Deploys::Web
   end
 
-  let(:librato_email) { "foo@foo.com" }
+  let(:librato_email) { 'foo@foo.com' }
   let(:librato_key) { '12345' }
-  let(:app) { 'foo' }
+  let(:api_endpoint) { "https://#{CGI.escape(librato_email)}:#{librato_key}@metrics-api.librato.com/v1" }
+  let(:app_name) { 'foo' }
   let(:version) { 'v75' }
   let(:t) { Time.now }
-  let(:id) { "#{app}.#{version}.#{t.to_i}" }
-  let(:params) { {:source => 'test'} }
+  let(:id) { "#{app_name}.#{version}.#{t.to_i}" }
+  let(:title) { "#{app_name}.#{version}" }
+  let(:source) { 'test' }
+  let(:params) { {:source => source} }
 
   before(:each) do
     Backstop::Deploys::Config.stub(:librato_email) { librato_email }
@@ -24,26 +27,49 @@ describe Backstop::Deploys::Web do
   describe 'PUT /deploys/:id' do
     context 'with proper id' do
       context 'with source' do
-        it 'should ask Librato for the proper annotation'
-
-        context 'non-existent annotation' do
-          it 'should create a new annotation'
-        end
 
         context 'existing annotation' do
-          it 'should update the existing annotation'
+          let(:event_id) { 12345 }
+          let(:stub_events) { [{:title => title, :source => source, :start_time => t.to_i, :id => event_id}] }
+          let(:stub_get_existing_body) { {:name => 'deploys', :events => [{source => stub_events}]} } 
+          let(:stub_get_existing_request) { stub_request(:get, "#{api_endpoint}/annotations/deploys").with(:query => { :sources => [source], :start_time => t.to_i }).to_return(:body => stub_get_existing_body.to_json)}
+          let(:stub_update_request) { stub_request(:put, "#{api_endpoint}/annotations/deploys/#{event_id}").with(:body => {:title => title}) }
+          before(:each) do
+            stub_get_existing_request
+            stub_update_request
+            put "/deploys/#{id}", params
+          end
+          it('should ask Librato for the proper annotation') { stub_get_existing_request.should have_been_made.once }
+          it('should update the existing annotation') { stub_update_request.should have_been_made.once }
+          it('should return a 200') { last_response.status.should eq(200) }
+        end
+
+        context 'non-existent annotation' do
+          let(:stub_get_missing_body) { {:name => 'deploys', :events => []} } 
+          let(:stub_get_missing_request) { stub_request(:get, "#{api_endpoint}/annotations/deploys").with(:query => { :sources => [source], :start_time => t.to_i }).to_return(:body => stub_get_missing_body.to_json)}
+          let(:stub_new_request) { stub_request(:put, "#{api_endpoint}/annotations/deploys").with(:body => "title=#{title}&start_time=#{t.to_i}") }
+          before(:each) do
+            stub_get_missing_request
+            stub_new_request
+            put "/deploys/#{id}", params
+          end
+          it('should ask Librato for the proper annotation') { stub_get_missing_request.should have_been_made.once }
+          it('should create a new annotation') { stub_new_request.should have_been_made.once }
+          it('should be 200') { last_response.status.should eq(200) }
         end
       end
 
       context 'without source' do
-        it 'should return a 400'
-        it 'should return a proper error message'
+        before(:each) { put "/deploys/#{id}", { :foo => :bar } }
+        it('should return a 400') { last_response.status.should eq(400) }
+        it('should return a proper error message') { JSON.parse(last_response.body).has_key?('message') }
       end
     end
 
     context 'with improperly formatted id' do
-      it 'should return a 400'
-      it 'should return a proper error message'
+      before(:each) { put "/deploys/foo.bar", params }
+      it('should return a 400') { last_response.status.should eq(400) }
+      it('should return a proper error message') { JSON.parse(last_response.body).has_key?('message') }
     end
   end
 end
